ed0e231891e97bee693334e3882aa5260b5dc253
Luong Pham Bao <58712033+19521242bao@users.noreply.github.com>
2021-02-02 14:41:33 +0700

    Update sosanhBT.py

--- sosanhBT.py
+++ sosanhBT.py
@@ -1,6 +1,4 @@
 import sys
-
-print(sys.version)
 import random
 import cProfile
 import time
@@ -20,20 +18,15 @@ C=createArray2D(n)
 s=n*(n-1)
 l=25
 countEdge=0
+print(f'so thanh pho: {n}')
 print(f'so canh: {l}')
 C=generateInputGraph(n,C,l,countEdge)
-v = [False for i in range(n)] 
+v = [False for i in range(n)]
 v[0] = True
 currPos,count,cost=0,1,0
-print(C)
 
 cProfile.run(f'tspbacktrack({C}, {currPos},{v}, {n}, {count}, {cost},{[]},{[]})')
-print(C)
-for i in range(l+1,s+1):
-  print(f'so canh: {i}')
-  C=generateInputGraph(n,C,i,i-1)#tăng so canh len 1
-  v = [False for i in range(n)] 
-  v[0] = True
-  currPos,count,cost=0,1,0
-
-  cProfile.run(f'tspbacktrack1({C}, {currPos},{v}, {n}, {count}, {cost},{[]},{[]})')
+v = [False for i in range(n)]
+v[0] = True
+currPos,count,cost=0,1,0
+cProfile.run(f'tspbacktrack1({C}, {currPos},{v}, {n}, {count}, {cost},{[]},{[]})')
